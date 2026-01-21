88b3b81a94491fddc8cb00e5edff92ebfe6b1b5f
jsndz <jaisondz9360@gmail.com>
2026-01-22 01:10:10 +0530

    add command(creating blob)

---
+++ docs/build.md
@@ -0,0 +1,10 @@
+# Neat is a clone of git
+
+
+## Stage 1 : neat init
+
+Git is Version control. It tracks changes overtime. 
+Everything about git is stored in .git which is a hidden file.
+Create a .git folder with refs and objects as sub-folders and a HEAD file.
+If you know linked list we name the main point as head. 
+This HEAD is similar it should point to something for now lets say HEAD points to "ref: refs/heads/main".
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
 )
 
 func main() {
@@ -11,7 +15,10 @@ func main() {
 	switch args[1] {
 	case "init":
 		initNeat()
-
+	case "add":
+		add(args[2])
+	case "cat-file":
+		cat(args[2], args[3])
 	default:
 		fmt.Println("Unknown command")
 		return
@@ -38,5 +45,56 @@ func initNeat() {
 		return
 	}
 	fmt.Println("Initialized neat repository")
+
+}
+
+func add(filename string) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error in getting working directory", err)
+		return
+	}
+	path := filepath.Join(cwd, filename)
+	content, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Error reading file", err)
+		return
+	}
+	size := len(content)
+	header := []byte("blob " + strconv.Itoa(size) + "\x00")
+	// \x00 is the null byte — a single byte with value 0.
+	blob := append(header, content...)
+	sha := sha1Hash(blob)
+	dir := sha[:2]
+	file := sha[2:]
+
+	objectsDir := filepath.Join(cwd, ".neat", "objects")
+	if err := os.MkdirAll(objectsDir, 0755); err != nil {
+		fmt.Println("Error in creating objects", err)
+		return
+	}
+
+	objDir := filepath.Join(objectsDir, dir)
+	if err := os.MkdirAll(objDir, 0755); err != nil {
+		fmt.Println("Error in creating object", err)
+		return
+	}
+
+	blobPath := filepath.Join(objDir, file)
+	if err := os.WriteFile(blobPath, blob, 0644); err != nil {
+		fmt.Println("Error in writing to file", err)
+		return
+	}
+	fmt.Println("Added object:", sha)
+
+}
+
+func sha1Hash(data []byte) string {
+	h := sha1.New()
+	h.Write(data)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func cat() {
 
 }
