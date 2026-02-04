a704f512a299d6d908ff090526bf9e087c472148
jsndz <jaisondz9360@gmail.com>
2026-02-04 12:36:54 +0530

    branch command

--- src/commands/clone.go
+++ src/commands/clone.go
@@ -1,5 +1,14 @@
 package commands
 
-func Clone(url string) {
+import (
+	"github.com/jsndz/neat/src/utils"
+)
 
+func Clone(url string) {
+	utils.Download(url, "neat.zip")
+	utils.Unzip("neat.zip", ".")
+	commitSha := utils.GetCurrentBranchCommitSha()
+	if commitSha != "" {
+		utils.RestoreFromCommit(commitSha)
+	}
 }
